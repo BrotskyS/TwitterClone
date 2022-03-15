57d75cea6c7cdba4d9574ad6421ab03ef92f06ad
BrotskyS <sergiypik.work@gmail.com>
2022-03-15 09:50:07 +0200

    frofile view

---
+++ TwitterClone/Core/Messages/MessagesView.swift
@@ -0,0 +1,20 @@
+//
+//  MessagesView.swift
+//  TwitterClone
+//
+//  Created by Sergiy Pik on 14.03.2022.
+//
+
+import SwiftUI
+
+struct MessagesView: View {
+    var body: some View {
+        Text("messages")
+    }
+}
+
+struct MessagesView_Previews: PreviewProvider {
+    static var previews: some View {
+        MessagesView()
+    }
+}
---
+++ TwitterClone/Core/Notification/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  Notification.swift
+//  TwitterClone
+//
+//  Created by Sergiy Pik on 14.03.2022.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct Notification_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
